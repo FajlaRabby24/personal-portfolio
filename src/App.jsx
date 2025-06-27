28a9f9b9697900a5daa66ce756aa145dec47a0a2
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-06-28 00:40:05 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="bg-primary min-h-screen">
+      <header className="backdrop-blur-2xl sticky top0 border-b border-[#ededed54]">
+        <Navbar />
+      </header>
+      <main></main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <a className="text-white font-semibold " href="#">
+          Home
+        </a>
+      </li>
+      <li>
+        <a className="text-white font-semibold " href="#">
+          About
+        </a>
+      </li>
+      <li>
+        <a className="text-white font-semibold " href="#">
+          Skills
+        </a>
+      </li>
+      <li>
+        <a className="text-white font-semibold " href="#">
+          Projects
+        </a>
+      </li>
+      <li>
+        <a className="text-white font-semibold " href="#">
+          Contact
+        </a>
+      </li>
+    </>
+  );
+
+  return (
+    <nav className="max-w-7xl mx-auto ">
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">
+            <h3 className="font-bold text-5xl text-white title">FR</h3>
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-5">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
