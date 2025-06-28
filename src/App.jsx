98c9485eb756eefcbb371a8a403e6a01319b9451
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-06-29 01:29:56 +0600

    skills section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import AboutMe from "./components/AboutMe";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
-    <div className="bg-primary min-h-screen">
+    <div className="min-h-screen">
       <header className="backdrop-blur-2xl sticky top0 border-b border-[#ededed54]">
         <Navbar />
       </header>
-      <main className=" space-y-28">
+      <main className=" space-y-40">
         <Banner />
         <AboutMe />
+        <Skills />
       </main>
       <footer>
         <Footer />
---
+++ src/components/Skills.jsx
@@ -0,0 +1,60 @@
+import bootstrap from "../assets/images/bootstrap.png";
+import css from "../assets/images/css.png";
+import express from "../assets/images/express.png";
+import figma from "../assets/images/figma.png";
+import firebase from "../assets/images/firebase.png";
+import git from "../assets/images/git.png";
+import github from "../assets/images/github.png";
+import html from "../assets/images/html.png";
+import js from "../assets/images/js.png";
+import jwt from "../assets/images/jwt.png";
+import mongodb from "../assets/images/mongodb.png";
+import mui from "../assets/images/mui.png";
+import nodejs from "../assets/images/nodejs.png";
+import react from "../assets/images/react.png";
+import tailwind from "../assets/images/tailwind.png";
+import vscode from "../assets/images/vscode.png";
+import Container from "./Container";
+
+const skills = [
+  { image: bootstrap, name: "Bootstrap" },
+  { image: css, name: "CSS" },
+  { image: express, name: "Express" },
+  { image: figma, name: "Figma" },
+  { image: firebase, name: "Firebase" },
+  { image: git, name: "Git" },
+  { image: github, name: "Github" },
+  { image: html, name: "HTML" },
+  { image: js, name: "JavaScript" },
+  { image: mongodb, name: "Mongodb" },
+  { image: mui, name: "Material UI" },
+  { image: nodejs, name: "Node" },
+  { image: react, name: "React" },
+  { image: tailwind, name: "Tailwind" },
+  { image: vscode, name: "Vs Code" },
+  { image: jwt, name: "JWT" },
+];
+
+const Skills = () => {
+  return (
+    <Container>
+      <section className="pb-20">
+        <h1 className="section-title">Skills</h1>
+        <div className="grid grid-cols-8  items-center gap-3">
+          {skills.map((skill, idx) => (
+            <div key={idx} className="text-center  ">
+              <img
+                className="min-h-[96px] max-h-[96px] mx-auto"
+                src={skill.image}
+                alt={`${skill.name}.png`}
+              />
+              <h3 className="text-white mt-3">{skill.name}</h3>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Skills;
